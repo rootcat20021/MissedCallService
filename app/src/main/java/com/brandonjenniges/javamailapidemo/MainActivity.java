cbcae0f9bcec259f3b5bae4ed1c1366c523b08d5
rootcat_20021 <pushkar.sareen@gmail.com>
2018-09-04 16:50:23 +0530

    Added better lifecycle debug messages to Main Activity. Next stop: Better lifecycle messages to Service

--- app/src/main/java/com/brandonjenniges/javamailapidemo/MainActivity.java
+++ app/src/main/java/com/brandonjenniges/javamailapidemo/MainActivity.java
@@ -8,7 +8,10 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.PowerManager;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.sql.Timestamp;
 
 import static android.support.v4.content.WakefulBroadcastReceiver.startWakefulService;
 
@@ -18,6 +21,8 @@ public class MainActivity extends Activity {
     // to startup the service
     private PowerManager.WakeLock wl;
     private static final String DNAME = "MissedCall";
+    private BufferedWriter WriterActivity;
+    private java.util.Date date = new java.util.Date();
     public void launchTestService() {
         // Construct our Intent specifying the Service
         System.out.println("Created Intent!");
@@ -42,17 +47,77 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         System.out.println("Created a MainActivity!");
         System.out.println("Launching service!");
+        try {
+            WriterActivity = new BufferedWriter(new FileWriter("/sdcard/Android/data/MissedCall/MainActiviy.txt", true));
+        } catch(Exception e) {
+        }
+
         File rootPath = new File("/sdcard/Android/data/", DNAME);
         if(!rootPath.exists()) {
             rootPath.mkdirs();
         }
+
+        try {
+            WriterActivity.write("\n---------------------------------\n");
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Created new Activity\nNow launching TestService\n");
+        } catch (Exception e) {}
         launchTestService();
+        try {
+            WriterActivity.write("Launched Service\n");
+        } catch (Exception e) {}
         System.out.println("Launched service!");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Activity about to be paused\n");
+        } catch(Exception e) {}
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        try {
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Activity resumed\n");
+        } catch(Exception e) {}
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        try {
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Activity stopped\n");
+        } catch (Exception e) {}
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        try {
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Activity restarted\n");
+        } catch (Exception e) {}
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        try {
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Activity about to be destroyed. Going to release wakelock\n");
+        } catch(Exception e) {}
         if (wl.isHeld()) wl.release();
+        try {
+            WriterActivity.write(new Timestamp(date.getTime()) + "\n");
+            WriterActivity.write("Wakelock released\n");
+            WriterActivity.close();
+        } catch(Exception e) {}
+
     }
 }
